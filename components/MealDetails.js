33d348102cc73af46e4449d48a1c9b53f2bea058
ashutosh <ashutosh@discoverwebtech.com>
2022-07-06 00:18:32 +0530

    Initial

---
+++ components/MealDetails.js
@@ -0,0 +1,37 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+
+const MealDetails = ({
+  duration,
+  complexity,
+  affordability,
+  style,
+  textStyle,
+}) => {
+  return (
+    <View style={[styles.details, style]}>
+      <Text style={[styles.detailitem, textStyle]}>{duration} m</Text>
+      <Text style={[styles.detailitem, textStyle]}>
+        {complexity.toUpperCase()}
+      </Text>
+      <Text style={[styles.detailitem, textStyle]}>
+        {affordability.toUpperCase()}
+      </Text>
+    </View>
+  );
+};
+
+export default MealDetails;
+
+const styles = StyleSheet.create({
+  details: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 8,
+  },
+  detailitem: {
+    marginHorizontal: 4,
+    fontSize: 12,
+  },
+});
---
+++ components/MealItem.js
@@ -0,0 +1,82 @@
+import { View, Text, Pressable, Image, StyleSheet } from "react-native";
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import MealDetails from "./MealDetails";
+
+const MealItem = ({
+  id,
+  title,
+  imageUrl,
+  duration,
+  complexity,
+  affordability,
+}) => {
+  const navigation = useNavigation();
+
+  const selectMealItemHandler = () => {
+    navigation.navigate("MealDetails", {
+      mealId: id,
+    });
+  };
+
+  return (
+    <View style={styles.mealItem}>
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={({ pressed }) => (pressed ? styles.buttonPressed : null)}
+        onPress={selectMealItemHandler}
+      >
+        <View style={styles.innerContainer}>
+          <View>
+            <Image source={{ uri: imageUrl }} style={styles.image} />
+            <Text style={styles.title}>{title}</Text>
+          </View>
+          <MealDetails
+            duration={duration}
+            affordability={affordability}
+            complexity={complexity}
+          />
+        </View>
+      </Pressable>
+    </View>
+  );
+};
+
+export default MealItem;
+
+const styles = StyleSheet.create({
+  mealItem: {
+    margin: 16,
+    borderRadius: 8,
+    overflow: "hidden",
+    backgroundColor: "white",
+    elevation: 4,
+  },
+  innerContainer: {
+    borderRadius: 8,
+    overflow: "hidden",
+  },
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  buttonPressed: {
+    opacity: 0.5,
+  },
+  title: {
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 18,
+    margin: 8,
+  },
+  details: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 8,
+  },
+  detailitem: {
+    marginHorizontal: 4,
+    fontSize: 12,
+  },
+});
